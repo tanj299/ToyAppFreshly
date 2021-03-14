12fda12051a970cccf0653e1d01e0ed2ea2acd0c
tanj299 <tanj299@gmail.com>
2021-03-14 14:13:34 -0400

    Added Micropost controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
+  # Code to create mapping of user posts to controlls actions for the Micropost resource
+  resources :microposts
   # Code to create mapping of user URLs to controller actions for the Users resource
   resources :users
   root 'users#index'
