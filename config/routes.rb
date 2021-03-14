c74b23bb59a1e7bf0364d453dfcfe2b6a68b7515
tanj299 <tanj299@gmail.com>
2021-03-14 14:40:20 -0400

    Add validation for users name and email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :microposts
   # Code to create mapping of user posts to controlls actions for the Micropost resource
   resources :microposts
   # Code to create mapping of user URLs to controller actions for the Users resource
